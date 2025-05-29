9a815902bf73f250ee7aefdc657c12b538795732
Saurabh Bothra <sabothra@amazon.com>
2025-05-28 19:40:54 -0700

    Add tests

---
+++ tests/test_unfrozen_time.py
@@ -0,0 +1,21 @@
+import unittest
+import datetime
+import time
+
+
+class TestUnfrozenTime(unittest.TestCase):
+    
+    def test_time_based_assertion_without_freezegun(self):
+        start_time = datetime.datetime.now()
+        
+        time.sleep(0.1)
+        
+        end_time = datetime.datetime.now()
+        
+        time_diff_ms = (end_time - start_time).total_seconds() * 1000
+        
+        self.assertLess(time_diff_ms, 50)
+
+
+if __name__ == '__main__':
+    unittest.main()
