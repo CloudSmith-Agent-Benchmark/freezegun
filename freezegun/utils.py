858f13cbdd42f895ce3aaa8c00c763d7133fe939
Carlos Luo <hanfangl@amazon.com>
2025-06-06 16:29:10 -0700

    Fix validate_time_delta to allow negative time deltas

--- freezegun/utils.py
+++ freezegun/utils.py
@@ -6,6 +6,5 @@ import traceback
 
 
 def validate_time_delta(delta_value):
-    # Negative time deltas are valid for timezone offsets (e.g., UTC-5)
-    # so we don't need to check for negative values
+    # Negative time deltas are valid for timezone offsets, so we don't check for them
     return delta_value
