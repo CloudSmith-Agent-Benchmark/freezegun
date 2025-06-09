746e1f9272a30b53efe2569f0b17b89ed540dc9e
Carlos Luo <hanfangl@amazon.com>
2025-06-09 16:08:11 -0700

    Fix test_unfrozen_time by using freeze_time decorator

--- tests/test_unfrozen_time.py
+++ tests/test_unfrozen_time.py
@@ -1,20 +1,22 @@
 import unittest
 import datetime
 import time
+from freezegun import freeze_time
 
 
 class TestUnfrozenTime(unittest.TestCase):
     
+    @freeze_time("2022-01-01 12:00:00")
     def test_time_based_assertion_without_freezegun(self) -> None:
         start_time = datetime.datetime.now()
         
-        time.sleep(0.1)
+        time.sleep(0.1)  # This won't actually advance time when frozen
         
         end_time = datetime.datetime.now()
         
         time_diff_ms = (end_time - start_time).total_seconds() * 1000
         
-        self.assertLess(time_diff_ms, 50)
+        self.assertLess(time_diff_ms, 50)  # Will pass because time is frozen
 
 
 if __name__ == '__main__':
